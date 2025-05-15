e4c6b2980a97cfce58bfe4e50fa9d6bd466d3ad8
Cláudio Crispim Campos Júnior <105263167+claudi0jr@users.noreply.github.com>
2025-05-15 14:44:36 -0300

    feat: adicionado laboratorioRouts

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.set('json spaces', 2);
-require('dotenv').config();
+app.set("json spaces", 2);
+require("dotenv").config();
 
 // Rotas
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const labsRoutes = require("./routes/laboratorioRoutes");
 
 // Middlewares
 app.use(express.json());
 
 // Registro das rotas
-app.use('/', authRoutes);
-app.use('/user', userRoutes);
+app.use("/", authRoutes);
+app.use("/user", userRoutes);
+app.use("/laboratorio", labsRoutes);
 //app.use('/laboratorio', labsRoutes);
 
 module.exports = app;
