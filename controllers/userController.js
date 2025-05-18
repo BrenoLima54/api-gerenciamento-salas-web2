271226242e3376a1a858b2deab9a1b6f18c96526
Nath <playerxmatt@gmail.com>
2025-05-18 20:16:39 -0300

    fix: make /user/registrar return 400 if user already exists

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 exports.create = async (req, res) => {
   const { email, senha } = req.body;
@@ -12,7 +12,7 @@ exports.create = async (req, res) => {
   try {
     const usuarioExistente = await User.findOne({ email });
     if (usuarioExistente) {
-      return res.status(409).json({ erro: 'Usuário já existe' });
+      return res.status(400).json({ erro: 'Usuário já existe' });
     }
 
     const senhaHash = await bcrypt.hash(senha, 10);
@@ -30,29 +30,29 @@ exports.logar = async (req, res) => {
   const { email, senha } = req.body;
 
   if (!email || !senha) {
-    return res.status(400).json({ erro: "E-mail e senha são obrigatórios!" });
+    return res.status(400).json({ erro: 'E-mail e senha são obrigatórios!' });
   }
 
   try {
     const usuario = await User.findOne({ email });
     if (!usuario) {
-      return res.status(401).json({ erro: "Credenciais inválidas" });
+      return res.status(401).json({ erro: 'Credenciais inválidas' });
     }
 
     const senhaValida = await bcrypt.compare(senha, usuario.senha);
     if (!senhaValida) {
-      return res.status(401).json({ erro: "Credenciais inválidas" });
+      return res.status(401).json({ erro: 'Credenciais inválidas' });
     }
 
     const token = jwt.sign(
       { id: usuario._id, email: usuario.email },
       process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: '1h' }
     );
 
     res.json({ token });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ erro: "Erro interno no servidor" });
+    res.status(500).json({ erro: 'Erro interno no servidor' });
   }
 };
