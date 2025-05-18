0fbe69ee5e5d2e86d77f7d501b09970dcf076c92
Nath <playerxmatt@gmail.com>
2025-05-18 20:17:13 -0300

    test: add /user tests

---
+++ __tests__/mocks/user.mock.js
@@ -0,0 +1,15 @@
+exports.userWithoutId = {
+  email: 'example@email.com',
+  senha: '123123',
+};
+
+exports.userWithoutPassword = {
+  _id: '123',
+  email: 'example@email.com',
+};
+
+exports.user = {
+  _id: '123',
+  email: 'example@email.com',
+  senha: '123123',
+};
